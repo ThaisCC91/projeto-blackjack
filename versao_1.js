62d363fc1475e35011cf55e44bdfd1b396b44348
Thais Contecote Christofalo <thaiscc@MacBook-Air-de-Thais.local>
2023-10-08 15:37:05 -0300

    blackjack versão 1.0

--- versao_1.js
+++ versao_1.js
@@ -10,3 +10,25 @@
  * 
  * 
  */
+console.log ("Boas vindas ao jogo de Blackjack!")
+comprarCarta()
+
+if(confirm("Deseja iniciar uma nova rodada?)) {
+   let carta01Usuario = comprarCarta()
+   let carta02Usuario - comprarCarta()
+   let valorUsario = carta01Usuario.valor + carta02Usuario.valor
+   console.log(`Usuario - cartas: ${carta01Usuario.texto} ${carta02Usuario.texto} - pontuacao: ${valorUsuario}`)
+   let carta01Pc = comprarCarta()
+   let carta02Pc = comprarCarta()
+   let valorPc = carta01Pc.valor + carta02Pc.valor
+   console.log(`Pc - cartas: ${carta01Pc.texto} ${carta02Pc.texto} - pontuacao: ${valorPc}`)
+   if ((valorUsario <= 21) && (valorUsario > valorPc)){
+      console.log("Usuario ganhou!")
+   } else if (valorUsario === valorPc){
+      console.log("Empate!")
+   } else { 
+      console.log("Pc ganhou!")
+   }
+} else {
+   console.log("Fim de jogo!")
+}
