85b837cf48d113d1abdd35cb776f895e5938f04f
Thais Christofalo <142272648+ThaisCC91@users.noreply.github.com>
2023-10-30 11:28:05 -0300

    Add files via upload

--- versao_1.js
+++ versao_1.js
@@ -11,24 +11,37 @@
  * 
  */
 console.log ("Boas vindas ao jogo de Blackjack!")
-comprarCarta()
-
-if(confirm("Deseja iniciar uma nova rodada?)) {
-   let carta01Usuario = comprarCarta()
-   let carta02Usuario - comprarCarta()
-   let valorUsario = carta01Usuario.valor + carta02Usuario.valor
-   console.log(`Usuario - cartas: ${carta01Usuario.texto} ${carta02Usuario.texto} - pontuacao: ${valorUsuario}`)
-   let carta01Pc = comprarCarta()
-   let carta02Pc = comprarCarta()
-   let valorPc = carta01Pc.valor + carta02Pc.valor
-   console.log(`Pc - cartas: ${carta01Pc.texto} ${carta02Pc.texto} - pontuacao: ${valorPc}`)
-   if ((valorUsario <= 21) && (valorUsario > valorPc)){
-      console.log("Usuario ganhou!")
-   } else if (valorUsario === valorPc){
-      console.log("Empate!")
-   } else { 
-      console.log("Pc ganhou!")
+function comprarCarta() {
+   const cartas = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+   const naipes = ["♦️", "♥️", "♣️", "♠️"];
+   const numero = cartas[Math.floor(Math.random() * 13)];
+   const naipe = naipes[Math.floor(Math.random() * 4)];
+   let valor;
+   if (numero === "A") {
+   valor = 11;
+}  else if (numero === "J" || numero === "Q" || numero === "K") {
+   valor = 10;
+} else {
+   valor = parseInt(numero);
+}
+return { texto: numero + naipe, valor: valor };
+}
+if (confirm("Deseja iniciar uma nova rodada?")) {
+   let carta01Usuario = comprarCarta();
+   let carta02Usuario = comprarCarta();
+   let valorUsuario = carta01Usuario.valor + carta02Usuario.valor;
+   console.log(`Usuario - cartas: ${carta01Usuario.texto} ${carta02Usuario.texto} - pontuação: ${valorUsuario}`);
+   let carta01Pc = comprarCarta();
+   let carta02Pc = comprarCarta();
+   let valorPc = carta01Pc.valor + carta02Pc.valor;
+   console.log(`Pc - cartas: ${carta01Pc.texto} ${carta02Pc.texto} - pontuação: ${valorPc}`);
+   if (valorUsuario <= 21 && valorUsuario > valorPc) {
+       console.log("Usuário ganhou!");
+   } else if (valorUsuario === valorPc) {
+       console.log("Empate!");
+   } else {
+       console.log("Pc ganhou!");
    }
 } else {
-   console.log("Fim de jogo!")
+   console.log("Fim de jogo!");
 }
--- versao_2.js
+++ versao_2.js
@@ -10,69 +10,77 @@
  * 
  * 
  */
-compraCarta()
-const cartaUsuario = [compraCarta(), compraCarta()]
-const cartaPc = [compraCarta(), compraCarta()]
+    function comprarCarta(){
+      const cartas = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    const naipes = ["♦️", "♥️", "♣️", "♠️"];
+    const numero = cartas[Math.floor(Math.random() * 13)];
+    const naipe = naipes[Math.floor(Math.random() * 4)];
+    let valor;
+    if (numero === "A") {
+        valor = 11;
+    } else if (numero === "J" || numero === "Q" || numero === "K") {
+        valor = 10;
+    } else {
+        valor = parseInt(numero);
+    }
+    return { texto: numero + naipe, valor: valor };
+    }
+
+const cartaUsuario = [comprarCarta(), comprarCarta()];
+const cartaPc = [comprarCarta(), comprarCarta()];
 
 if (confirm("Bem vindo ao jogo de BlackJack!\nDeseja iniciar uma nova rodada?")){
-   let valorUsuario = cartaUsuario[0].valor + cartaUsuario[1].valor
-   let valorPc = cartaPc[0].valor + cartaPc[1].valor
+   let valorUsuario = cartaUsuario[0].valor + cartaUsuario[1].valor;
+   let valorPc = cartaPc[0].valor + cartaPc[1].valor;
 
-   let mensagem = confirm(`As cartas sao ${cartaUsuario[0].texto} ${cartaUsuario[1].texto}. A carta mostrada pelo computador é ${cartaPc[0].texto}\nDeseja outra carta?`)
+   let mensagem = confirm(`As cartas sao ${cartaUsuario[0].texto} ${cartaUsuario[1].texto}. A carta mostrada pelo computador é ${cartaPc[0].texto}\nDeseja outra carta?`);
 
    while (mensagem && valorUsuario <= 21){
-      const novaCarta = compraCarta()
-      cartaUsuario.push(novaCarta)
-      valorUsuario ÷= novaCarta.valor
+      const novaCarta = comprarCarta();
+      cartaUsuario.push(novaCarta);
+      valorUsuario += novaCarta.valor;
 
       let novasCartasUsuario = cartaUsuario.map((novaCarta) => {
-         return novaCarta.texto
-      })
-      mensagem = confirm(`As cartas sao ${novasCartasUsuario}. A carta mostrada pelo computador é ${cartaPc[0].texto}\nDeseja outra carta?`)
-      
+         return novaCarta.texto;
+      });
+      mensagem = confirm(`As cartas sao ${novasCartasUsuario}. A carta mostrada pelo computador é ${cartaPc[0].texto}\nDeseja outra carta?`);
    }
-   let compraCartaPc = compraCarta()
-
+  
    while (valorPc <= 21){
-      const novaCarta = compraCarta()
-      cartaPc.push(novaCarta)
-      valorPc ÷= novaCarta.valor
-
-      compraCartaPc++
-
-      let novaCartaPc = cartaPc.map((novaCarta) => {
-         return novaCarta.texto
-      })
+      const novaCarta = comprarCarta();
+      cartaPc.push(novaCarta);
+      valorPc += novaCarta.valor;
    }
-   if ((valorUsuario <= 21) || (valorUsuario < valorPc)) {
+
+   if ((valorUsuario <= 21) && (valorUsuario > valorPc || valorPc > 21)) {
       let novasCartasUsuario = cartaUsuario.map((novaCarta) => {
-         return novaCarta.texto
-      })
+         return novaCarta.texto;
+      });
       let novaCartaPc = cartaPc.map((novaCarta) => {
-         return novaCarta.texto
-      }) 
-      alert(`usuario - cartas: ${novasCartasUsuario} - Pontuacao: ${valorUsuario}\nComputador - cartas: ${novaCartaPc} - Pontuacao: ${valorPc}\nUsuario ganhou!`)
+         return novaCarta.texto;
+      }); 
+      alert(`usuario - cartas: ${novasCartasUsuario} - Pontuacao: ${valorUsuario}\nComputador - cartas: ${novaCartaPc} - Pontuacao: ${valorPc}\nUsuario ganhou!`);
       
    } else if (valorUsuario === valorPc){
       let novasCartasUsuario = cartaUsuario.map((novaCarta) => {
-         return novaCarta.texto
-      })
+         return novaCarta.texto;
+      });
       let novaCartaPc = cartaPc.map((novaCarta) => {
-         return novaCarta.texto
-      })
-      alert (`usuario - cartas: ${novasCartasUsuario} - pontuacao: ${valorUsuario}\nComputador - cartas: ${novaCartaPc} - Pontuacao: ${valorPc}\nEmpate!`)
+         return novaCarta.texto;
+      });
+      alert (`usuario - cartas: ${novasCartasUsuario} - pontuacao: ${valorUsuario}\nComputador - cartas: ${novaCartaPc} - Pontuacao: ${valorPc}\nEmpate!`);
 
    } else {
       let novasCartasUsuario = cartaUsuario.map((novaCarta) => {
-         return novaCarta.texto
-      })
-      let novaCartaPc = cartaPc.map((novaCarta) =>{
-         return novaCarta.texto
-      })
-      alert (`usuario - cartas: ${novasCartasUsuario} - pontuacao: ${valorUsuario}\nCompuatdor - cartas: ${novaCartaPc} - Pontuacao: ${valorPc}\nComputador ganhou!`)
+         return novaCarta.texto;
+      });
+      let novaCartaPc = cartaPc.map((novaCarta) => {
+         return novaCarta.texto;
+      });
+      alert (`usuario - cartas: ${novasCartasUsuario} - pontuacao: ${valorUsuario}\nCompuatdor - cartas: ${novaCartaPc} - Pontuacao: ${valorPc}\nComputador ganhou!`);
    }
 
 } else {
-   alert ("Fim de jogo!")
+   alert ("Fim de jogo!");
 }
    
